c560b3d5b7e359c5c356e9cd3dc587bbdb596e17
Lucas Ricciardi <lucas.ricciardi@outlook.com>
2017-07-19 18:21:53 -0300

    grafos

--- main.py
+++ main.py
@@ -1,185 +1,46 @@
 # -*- coding: utf-8 -*-
 
-import sys
-import math
+import pygame
+import time
 
-class StateMachine(object):
+white = (0xff, 0xff, 0xff)
+blue = (0x00, 0x00, 0xff)
 
-    def __init__(self, state):
-        self.startState = state
-    
-    def name(self):
-        return type(self).__name__
 
-    def reset(self):
-        self.state = self.startState
+def main():
+    pygame.init()
 
-    def getNextValues(self, state, inp):
-        nextState = self.nextState(state, inp)
-        return (nextState, nextState)
+    pygame.display.set_caption('App')
+    screen = pygame.display.set_mode([640, 480])
 
-    def done(self, state):
-        return False
+    sqr = pygame.Surface([30, 50])
+    sqr_rect = sqr.get_rect()
 
-    def transduce(self, inp, verbose=False):
-        self.reset()
-        output = []
-        if verbose:
-            print('\n{}'.format(self.name()))
-            print('Start state: {}'.format(self.state))
-        for i in inp:
-            if not self.done(self.state):
-                (s, o) = self.getNextValues(self.state, i)
-                if verbose:
-                    print('In: {input} Out: {output} Next State: {state}'
-                        .format(input=i, output=o, state=s))
-                self.state = s
-                output.append(o)
-        return output
+    dx = 1
+    dy = 2
 
-    def run(self, n=10, verbose=False):
-        return self.transduce([None] * n, verbose)
+    done = False
+    while not done:
 
-class Cascade(StateMachine):
+        for evt in pygame.event.get():
 
-    def __init__(self, sm1, sm2):
-        self.m1 = m1 = sm1
-        self.m2 = m2 = sm2
-        super(Cascade, self).__init__((m1.startState, m2.startState), name)
+            if evt.type == pygame.QUIT:
+                done = True
+        
+        sqr_rect.x += dx
+        if sqr_rect.x < 0 or sqr_rect.x > 640:
+            dx *= -1
 
-    def getNextValues(self, state, inp):
-        (s1, s2) = state 
-        (newS1, o1) = self.m1.getNextValues(s1, inp)
-        (newS2, o2) = self.m2.getNextValues(s2, o1)
-        return ((newS1, newS2), o2)
+        sqr_rect.y += dy
+        if sqr_rect.y < 0 or sqr_rect.y > 480:
+            dy *= -1
 
-class RepeatUntil(StateMachine):
+        screen.fill(white)
 
-    def __init__(self, sm, condition):
-        self.sm = sm
-        self.condition = condition
-        super(RepeatUntil, self).__init__((False, sm.startState))
+        screen.fill(blue, sqr_rect)
 
-    def getNextValues(self, state, inp):
-        (condTrue, smState) = state
-        (smState, o) = self.sm.getNextValues(smState, inp)
-        condTrue = self.condition(inp)
-        if self.sm.done(smState) and not condTrue:
-            smState = self.sm.startState
-        return((condTrue, smState), o)
-
-    def done(self, state):
-        (condTrue, smState) = state
-        return self.sm.done(smState) and condTrue
-
-class ConsumeFiveValues(StateMachine):
-
-    def __init__(self, state=(0, 0)):
-        super(ConsumeFiveValues, self).__init__(state)
-
-    def getNextValues(self, state, inp):
-        (count, total) = state
-        if count == 4:
-            return((count + 1, total + inp), total + inp)
-        else:
-            return((count + 1, total + inp), None)
-
-    def done(self, state):
-        (count, total) = state
-        return count == 5
-
-class Repeat(StateMachine):
-
-    def __init__(self, sm, n=None):
-        self.sm = sm
-        self.n = n
-        super(Repeat, self).__init__((0, sm.startState))
-
-    def advanceIfDone(self, counter, smState):
-        while self.sm.done(smState) and not self.done((counter, smState)):
-            counter += 1
-            smState = self.sm.startState
-        return (counter, smState)
-
-    def getNextValues(self, state, inp):
-        (counter, smState) = state
-        (smState, o) = self.sm.getNextValues(smState, inp)
-        (counter, smState) = self.advanceIfDone(counter, smState)
-        return ((counter, smState), o)
-
-    def done(self, state):
-        (counter, smState) = state
-        return counter == self.n
-
-class CharTSM(StateMachine):
-    
-    def __init__(self, c):
-        self.c = c
-        super(CharTSM, self).__init__(False)
-
-    def getNextValues(self, state, inp):
-        return (True, self.c)
-
-    def done(self, state):
-        return state
-
-class Sequence(StateMachine):
-
-    def __init__(self, smList):
-        self.smList = smList
-        self.n = len(smList)
-        super(Sequence, self).__init__((0, smList[0].startState))
-
-    def advanceIfDone(self, counter, smState):
-        while self.smList[counter].done(smState) and counter + 1 < self.n:
-            counter = counter + 1
-            smState = self.smList[counter].startState
-        return (counter, smState)
-
-    def getNextValues(self, state, inp):
-        (counter, smState) = state
-        (smState, o) = self.smList[counter].getNextValues(smState, inp)
-        (counter, smState) = self.advanceIfDone(counter, smState)
-        return ((counter, smState), o)
-
-    def done(self, state):
-        (counter, smState) = state
-        return self.smList[counter].done(smState)
-
-def makeTextSequenceTSM(string):
-    return Sequence([CharTSM(c) for c in string])
-
-class RepeatUntil(StateMachine):
-
-    def __init__(self, condition, sm):
-        self.sm = sm
-        self.condition = condition
-        super(RepeatUntil, self).__init__((False, sm.startState))
-
-    def getNextValues(self, state, inp):
-        (condTrue, smState) = state
-        (smState, o) = self.sm.getNextValues(smState, inp)
-        condTrue = self.condition(inp)
-        if self.sm.done(smState) and not condTrue:
-            smState = self.sm.startState
-        return ((condTrue, smState), o)
-
-    def done(self, state):
-        (condTrue, smState) = state
-        return self.sm.done(smState) and condTrue
-
-def main(argc, argv):
-    a = CharTSM('a')
-    print a.run(verbose=True)
-
-    a4 = Repeat(a, 4)
-    print a4.run(verbose=True)
-
-    m = makeTextSequenceTSM('Hello, World !')
-    print m.run(20, verbose=True)
-
-    t = RepeatUntil(lambda x: x > 10, ConsumeFiveValues())
-    print t.transduce(range(20), verbose=True)
+        pygame.display.flip()
+    pygame.quit()
 
 if __name__ == '__main__':
-    main(len(sys.argv), sys.argv)
+    main()
